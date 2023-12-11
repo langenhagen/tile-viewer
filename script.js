930b1eecf8ee4f17bb534985cc587b32088f3524
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-12-11 21:34:31 +0100

    Run `prettier` formatting on the code

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let originalImageHeight;
 
 // Reset image to its original size.
 function resetToOriginalSize() {
-  const background = document.querySelector('.background');
+  const background = document.querySelector(".background");
   if (originalImageWidth && originalImageHeight) {
     background.style.backgroundSize = `${originalImageWidth}px ${originalImageHeight}px`;
   }
@@ -29,33 +29,33 @@ function loadImage(imagePath) {
 }
 
 // Set the initial image from the CSS file.
-document.addEventListener('DOMContentLoaded', () => {
-  const initialImagePath = 'default.jpg';
+document.addEventListener("DOMContentLoaded", () => {
+  const initialImagePath = "default.jpg";
   loadImage(initialImagePath);
   resetToOriginalSize();
-  document.querySelector('.background').style.backgroundImage = `url(${initialImagePath})`;
+  document.querySelector(".background").style.backgroundImage = `url(${initialImagePath})`;
 });
 
-document.querySelector('.background').addEventListener('mousedown', (e) => {
+document.querySelector(".background").addEventListener("mousedown", (e) => {
   isDragging = true;
   initialMouseX = e.clientX;
   initialMouseY = e.clientY;
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener("mouseup", () => {
   isDragging = false;
 });
 
 // Panning functionality.
-document.addEventListener('mousemove', (e) => {
+document.addEventListener("mousemove", (e) => {
   if (isDragging) {
     const deltaX = e.clientX - initialMouseX;
     const deltaY = e.clientY - initialMouseY;
 
-    const background = document.querySelector('.background');
-    const currentBackgroundPosition = getComputedStyle(background).backgroundPosition.split(' ');
-    const currentBackgroundPositionX = parseFloat(currentBackgroundPosition[0].replace('px', ''));
-    const currentBackgroundPositionY = parseFloat(currentBackgroundPosition[1].replace('px', ''));
+    const background = document.querySelector(".background");
+    const currentBackgroundPosition = getComputedStyle(background).backgroundPosition.split(" ");
+    const currentBackgroundPositionX = parseFloat(currentBackgroundPosition[0].replace("px", ""));
+    const currentBackgroundPositionY = parseFloat(currentBackgroundPosition[1].replace("px", ""));
 
     background.style.backgroundPositionX = `${currentBackgroundPositionX + deltaX}px`;
     background.style.backgroundPositionY = `${currentBackgroundPositionY + deltaY}px`;
@@ -66,20 +66,20 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Zooming functionality.
-document.addEventListener('wheel', (e) => {
+document.addEventListener("wheel", (e) => {
   const delta = e.deltaY;
   const scaleChange = delta > 0 ? 1.1 : 0.9;
 
-  const background = document.querySelector('.background');
-  const currentBackgroundSize = getComputedStyle(background).backgroundSize.split(' ');
-  const currentBackgroundSizeX = parseFloat(currentBackgroundSize[0].replace('px', ''));
+  const background = document.querySelector(".background");
+  const currentBackgroundSize = getComputedStyle(background).backgroundSize.split(" ");
+  const currentBackgroundSizeX = parseFloat(currentBackgroundSize[0].replace("px", ""));
   const newBackgroundSizeX = currentBackgroundSizeX * scaleChange;
 
   background.style.backgroundSize = `${newBackgroundSizeX}px auto`;
 });
 
 // Change background file.
-document.getElementById('file-input').addEventListener('change', async (e) => {
+document.getElementById("file-input").addEventListener("change", async (e) => {
   const fileInput = e.target;
   const selectedFile = fileInput.files[0];
 
@@ -88,7 +88,7 @@ document.getElementById('file-input').addEventListener('change', async (e) => {
     reader.onload = async function (event) {
       const newImagePath = event.target.result;
       await loadImage(newImagePath);
-      document.querySelector('.background').style.backgroundImage = `url(${newImagePath})`;
+      document.querySelector(".background").style.backgroundImage = `url(${newImagePath})`;
       resetToOriginalSize();
     };
 
@@ -97,10 +97,10 @@ document.getElementById('file-input').addEventListener('change', async (e) => {
 });
 
 // Keyboard shortcut listener.
-document.addEventListener('keydown', (e) => {
-  if (e.ctrlKey && e.key === 'o') {
-    document.getElementById('file-input').click();
-  } else if (e.ctrlKey && e.key === '0') {
+document.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && e.key === "o") {
+    document.getElementById("file-input").click();
+  } else if (e.ctrlKey && e.key === "0") {
     resetToOriginalSize();
   }
 });
